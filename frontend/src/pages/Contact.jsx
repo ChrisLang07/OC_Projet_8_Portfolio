a7fed83dbbe3d930d87052ede767880b1953efec
ChrisLang07 <christophe.langlois.one@gmail.com>
2024-10-30 16:58:08 +0100

    Updating the fetch URL in Contact.jsx

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -22,7 +22,7 @@ export default function Contact() {
     const handleSubmit = async (event) => {
         event.preventDefault(); 
         try {
-            const response = await fetch('http://localhost:4000/api/contact', {
+            const response = await fetch('https://portfolio-backend-98ya.onrender.com/api/contact', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
