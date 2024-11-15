5cde76fc6b0e0c9ccf48442456817d3ab99c67fb
ChrisLang07 <christophe.langlois.one@gmail.com>
2024-11-15 10:52:43 +0100

    CSS optimization

--- frontend/src/pages/NoPage.jsx
+++ frontend/src/pages/NoPage.jsx
@@ -6,7 +6,6 @@ export default function NoPage() {
         <div className="nopage">
              <h1 className='number'>404</h1>
              <span className='oups'>Oups! Nothing here...</span>
-             <ArrowUp />
         </div>
     )
 };
