61ff1e59cb687bb47b5fab32781126ec1bd49f2c
ChrisLang07 <christophe.langlois.one@gmail.com>
2024-12-02 19:31:44 +0100

    NoPage.jsx's CSS rules / Container class added

--- frontend/src/pages/NoPage.jsx
+++ frontend/src/pages/NoPage.jsx
@@ -2,7 +2,7 @@ import '../assets/scss/components/NoPage.scss';
 
 export default function NoPage() {
     return (
-        <div className="nopage">
+        <div className="container nopage">
              <h1 className='number'>404</h1>
              <span className='oups'>Oups! Nothing here...</span>
         </div>
